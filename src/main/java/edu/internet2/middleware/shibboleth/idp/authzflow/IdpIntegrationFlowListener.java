8ecb253e985ab98bff066844b243db5d9caccc5c
Dmitriy Kopylenko <dima767@gmail.com>
2011-09-12 16:30:45 -0400

    Polishing

--- src/main/java/edu/internet2/middleware/shibboleth/idp/authzflow/IdpIntegrationFlowListener.java
+++ src/main/java/edu/internet2/middleware/shibboleth/idp/authzflow/IdpIntegrationFlowListener.java
@@ -11,16 +11,23 @@ public class IdpIntegrationFlowListener extends FlowExecutionListenerAdapter {
     @Override
     public void sessionStarted(RequestContext context, FlowSession session) {
         ServletContext servletContext = (ServletContext) context.getExternalContext().getNativeContext();
-        servletContext.getContext(session.getScope().getString("idpCallingContextName"))
-                .setAttribute(session.getScope().getString("idpCallingSessionId"), "UNAUTHORIZED");
+        ServletContext callingContext = servletContext.getContext(session.getScope().getString("idpCallingContextName"));
+        if (callingContext == null) {
+            throw new FlowExecutionException(context.getActiveFlow().getId(), context.getCurrentState().getId(),
+                    "Invalid entry into this flow. Probably used back button on the browser when flow has already ended.");
+        }
+        synchronized (servletContext) {
+            callingContext.setAttribute(session.getScope().getString("idpCallingSessionId"), "UNAUTHORIZED");
+        }
     }
 
     @Override
     public void sessionEnding(RequestContext context, FlowSession session, String outcome, MutableAttributeMap output) {
         ServletContext servletContext = (ServletContext) context.getExternalContext().getNativeContext();
-        servletContext.getContext(session.getScope().getString("idpCallingContextName"))
-                .setAttribute(session.getScope().getString("idpCallingSessionId"), "AUTHORIZED");
+        synchronized (servletContext) {
+            String status = "continue".equals(outcome) ? "AUTHORIZED" : "UNAUTHORIZED";
+            servletContext.getContext(session.getScope().getString("idpCallingContextName"))
+                    .setAttribute(session.getScope().getString("idpCallingSessionId"), status);
+        }
     }
-
-
 }
